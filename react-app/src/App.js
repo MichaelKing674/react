b61a712027a810403a73fa218a93cc67bf2bb57b
MichaelKing674 <michaeljking@sky.com>
2022-08-30 16:50:45 +0100

    Pushing Day 1 Work

---
+++ react-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" id="info">
+      <h1>Michael King</h1>
+      <ul>
+        <li>42</li>
+        <li>Wallaby Way</li>
+        <li>Sydney</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app/src/Header.jsx
@@ -0,0 +1,19 @@
+let Header = () => {
+    return (
+        <header>
+            <nav className="navbar navbar-expand-sm">
+                <a
+                    href="https://www.qa.com"
+                    className="nav-brand"
+                    taget="_blank"
+                >
+                    <img alt="QA Logo" src="https://i.imgur.com/smguoM3.png" width="100"></img>
+                </a>
+                <a href="/" className="navbar-brand">
+                    Todo App
+                </a>
+            </nav>
+        </header>
+    )
+}
+export default Header;
