42b989610877bba8a732e8f02c0fe29474c1618b
MichaelKing674 <michaeljking@sky.com>
2022-09-01 18:09:09 +0100

    Added all work from day3 including 1 super App

---
+++ all-exercises/src/Intro.jsx
@@ -0,0 +1,16 @@
+
+function Intro() {
+    return (
+      <div className="Component" id="info">
+        <h1>Michael King</h1>
+        <ul>
+          <li>42</li>
+          <li>Wallaby Way</li>
+          <li>Sydney</li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default Intro;
+  
